aecbe91bdb6a1441a8c28e805d5172d47ebfbb28
fox3000foxy <fox3000foxy@gmail.com>
2026-03-04 10:18:59 +0100

    hidding logs on actions

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,49 @@
 import { App } from "./app";
 import { container } from "./container";
 
-const app: App = container.get("App");
 const actionMode = process.argv.includes("--action");
+const writeGenericActionFailure = () => {
+  process.stderr.write("Action failed\n");
+};
 
-app.init({ actionMode }).catch((err) => console.error(err));
+if (actionMode) {
+  const noop = () => undefined;
+  console.log = noop;
+  console.info = noop;
+  console.warn = noop;
+  console.error = noop;
+  console.debug = noop;
+}
+
+const app: App = container.get("App");
+
+app.init({ actionMode }).catch((err) => {
+  if (actionMode) {
+    writeGenericActionFailure();
+  } else {
+    console.error(err);
+  }
+  process.exitCode = 1;
+});
 
 process.on("uncaughtException", (error) => {
-  console.log("Oh my god, something terrible happened: ", error);
+  if (actionMode) {
+    writeGenericActionFailure();
+  } else {
+    console.log("Oh my god, something terrible happened: ", error);
+  }
+  process.exitCode = 1;
 });
 
 process.on("unhandledRejection", (error, promise) => {
-  console.log(
-    " Oh Lord! We forgot to handle a promise rejection here: ",
-    promise,
-  );
-  console.log(" The error was: ", error);
+  if (actionMode) {
+    writeGenericActionFailure();
+  } else {
+    console.log(
+      " Oh Lord! We forgot to handle a promise rejection here: ",
+      promise,
+    );
+    console.log(" The error was: ", error);
+  }
+  process.exitCode = 1;
 });
